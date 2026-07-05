32596d65073381e70af344d2c2a39c61ab8493cf
yvoc-exec <noreply@github.com>
2026-07-05 18:50:59 +0800

    test: inject request options for UI workflows

--- src/test/java/burp/testsupport/UiWorkflowFixtures.java
+++ src/test/java/burp/testsupport/UiWorkflowFixtures.java
@@ -2,9 +2,15 @@ package burp.testsupport;
 
 import burp.UniversalImporter;
 import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.http.RequestOptions;
 import burp.api.montoya.http.message.HttpRequestResponse;
 import burp.api.montoya.http.message.requests.HttpRequest;
+import burp.auth.OAuth2Manager;
+import burp.utils.RequestBuilder;
+import burp.utils.ScriptEngine;
 import burp.utils.ScriptMode;
+import burp.utils.SharedRequestPipeline;
+import org.mockito.Mockito;
 
 import java.util.function.Function;
 
@@ -13,7 +19,26 @@ public final class UiWorkflowFixtures {
     }
 
     public static UniversalImporter newImporter(MontoyaApi api) {
-        UniversalImporter importer = new UniversalImporter(api, ScriptMode.DISABLED, null);
+        UniversalImporter importer = new UniversalImporter(api, ScriptMode.DISABLED, null) {
+            @Override
+            protected SharedRequestPipeline createSharedRequestPipeline(MontoyaApi pipelineApi,
+                                                                        RequestBuilder requestBuilder,
+                                                                        ScriptEngine scriptEngine,
+                                                                        OAuth2Manager oauth2Manager) {
+                return SharedRequestPipeline.withRequestOptionsFactory(
+                        pipelineApi,
+                        requestBuilder,
+                        scriptEngine,
+                        oauth2Manager,
+                        null,
+                        timeout -> {
+                            RequestOptions options = Mockito.mock(RequestOptions.class);
+                            Mockito.when(options.withRedirectionMode(Mockito.any())).thenReturn(options);
+                            Mockito.when(options.withResponseTimeout(Mockito.anyInt())).thenReturn(options);
+                            return options;
+                        });
+            }
+        };
         return UiBridgeInstaller.install(importer);
     }
 
